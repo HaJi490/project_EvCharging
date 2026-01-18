c796719cb9c7322408ec684dd63763bade68056f
Haji490 <99gkwldnjs99@gmail.com>
2026-01-18 23:48:14 +0900

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <MSWComponent> */}
           {children}
-        {/* </MSWComponent> */}
       </body>
     </html>
   );
